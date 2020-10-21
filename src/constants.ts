fbec0350dfea957417c04615cf2d062e160261e9
luml <lumeiling@bonc.com.cn>
2020-10-21 11:01:12 +0800

    fix: too much rain :octocat:

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
-export const RAIN_CHANCE = 0.4;
+export const RAIN_CHANCE = 0.25; // 0.4 is too much rain 
 export const SCENES = ["day", "rain"];
 export const DAY_LENGTH = 50;
 export const NIGHT_LENGTH = 5;
