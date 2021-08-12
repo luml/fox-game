cd090bd67ccf58564cfca576bdce94efde8a32bc
luml <1520622848@qq.com>
2021-08-12 18:27:10 +0800

    fix: literal types

--- src/ui.ts
+++ src/ui.ts
@@ -1,12 +1,12 @@
-export const modFox = function modFox(state) {
+export const modFox = function modFox(state: string) {
   document.querySelector(".fox").className = `fox fox-${state}`;
 };
 
-export const modScene = function (state) {
+export const modScene = function (state: string) {
   document.querySelector(".game").className = `game ${state}`;
 };
 
-export const togglePoopBag = function togglePoopBag(show) {
+export const togglePoopBag = function togglePoopBag(show: boolean) {
   document.querySelector(".poop-bag").classList.toggle("hidden", !show);
 };
 
